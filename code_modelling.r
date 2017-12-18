da1de87ed30a79d4763b51a0bcf216351d350d5e
Junsok Huhh <anarinsk@gmail.com>
2017-12-18 11:38:49 +0900

    Make it public

--- code_modelling.r
+++ code_modelling.r
@@ -1,8 +1,7 @@
 # 12.16. 2017
-# Junsok Huhh
-#
+# Junsok Huhh / anarinsk@gmail.com
 
-# Load basic packages and RData
+# Load basic packages and RData ----
 library('tidyverse')
 library('wrapr')
 library('here')
@@ -13,51 +12,7 @@ library('here')
 
 here('analysis_IGM', 'df_munged.RData') %.>% load(.)
 
-# Clustering by k-modes ----
-
-library('klaR')
-# Change name of clustering data 
-df_answer_wide4 -> tdf
-as.data.frame(tdf) -> tdf 
-row.names(tdf) <- tdf$name
-tdf[, -c(1)] -> tdf
-tdf[is.na(tdf)] <- 3
-
-set.seed(12315)
-kmodes(tdf, 2, iter.max = 10, weighted = FALSE) -> clu_res 
-
-fviz_cluster(list(data = tdf, cluster = clu_res$clu))
-
-df_answer_wide4$kmodes <- clu_res$clu
-df_answer_wide4 %.>% count(., kmodes)
-df_answer_wide4 %.>% dplyr::select(., one_of('name', 'kmodes')) -> zdf5
-
-tfunc <- function(x){median(x, na.rm = TRUE)}
-
-df_answer_wide4[, -c(1)] %.>% 
-  group_by(., kmodes) %.>% 
-  summarise_all(., tfunc) -> zdf0
-
-zdf0 %.>% 
-  gather(., key = "q", value = "median", -c(clu)) %.>% 
-  spread(., key = clu, value = median) -> zdf1
-
-zdf1 %.>% 
-  rename(.,
-    q_id = q, clu1 = '1', clu2 = '2'
-  ) %.>% 
-  mutate(.,
-    dff = clu1 - clu2
-  ) -> zdf2
-
-zdf2 %.>% count(., dff)
-
-zdf2 %.>% filter(., abs(dff) >= 1) -> zdf3
-zdf3 %.>% count(., clu1)
-zdf3 %.>% count(., clu2)
-zdf2 %.>% filter(., dff %in% c(-1, 1.5)) -> zdf4
-
-# H clustering ----
+# H clustering / HC ----
 
 # Change name of clustering data 
 change_rownames <- function(df, this_names){
@@ -112,7 +67,7 @@ agnes_opt %.>% my_dendplot(., 3)
 fviz_nbclust(tdf, FUN = hcut, method = "wss")
 fviz_nbclust(tdf, FUN = hcut, method = "silhouette")
 gap_stat <- clusGap(tdf, FUN = hcut, nstart = 25, K.max = 10, B = 50)
-fviz_gap_stat(gap_stat)
+fviz_gap_stat(gap_stat) 
 
 # Cut tree into 3 groups
 sub_agnes <- cutree(as.hclust(agnes_opt), k = 3)
@@ -121,9 +76,10 @@ fviz_cluster(list(data = tdf, cluster = sub_agnes))
 
 # compute divisive hierarchical clustering
 diana <- diana(tdf)
-diana %.>% my_dendplot(., 3)
+diana$dc
 sub_diana <- cutree(as.hclust(diana), k = 3)
 fviz_cluster(list(data = tdf, cluster = sub_diana))
+diana %.>% my_dendplot(., 3)
 
 # Descrptive Analysis ----
 
@@ -170,7 +126,7 @@ count_group_dff <- function(df = df_desc){
            dff_12 = clu1 - clu2,
            dff_23 = clu2 - clu3,
            dff_13 = clu1 - clu3 
-    ) -> zdf2
+    ) 
   
   zdf2 %.>% count(., dff_12) %.>% rename(., g_dff = dff_12, n_12 = n) -> zdf12
   zdf2 %.>% count(., dff_13) %.>% rename(., g_dff = dff_13, n_13 = n) -> zdf13
@@ -194,3 +150,62 @@ zdf2 %.>%
             clu3 == 1 & dff_13 >=  3 | clu3 == 1 & dff_23 >=  3 |
             clu3 == 2 & dff_13 >=  2 | clu3 == 2 & dff_23 >=  2 
            ) -> zdf4
+
+
+zdf3 %.>% 
+  separate(., q_id, into = c("q", "id", "question"), sep = "_") %.>% 
+  dplyr::select(., -one_of('q')) %.>% 
+  mutate(., id = as.integer(id), question = as.integer(question)) %.>% 
+  arrange(., id, question) %.>% 
+  left_join(., df_survey, by = c('id')) %.>% 
+  rename(., url = list) -> zdf5 
+
+# ++++++++++++++++++++ ----
+# Experimental ----
+# ++++++++++++++++++++ ----
+
+# Clustering by k-modes ----
+
+library('klaR')
+# Change name of clustering data 
+df_answer_wide4 -> tdf
+as.data.frame(tdf) -> tdf 
+row.names(tdf) <- tdf$name
+tdf[, -c(1)] -> tdf
+tdf[is.na(tdf)] <- 3
+
+set.seed(12315)
+kmodes(tdf, 2, iter.max = 10, weighted = FALSE) -> clu_res 
+
+fviz_cluster(list(data = tdf, cluster = clu_res$clu))
+
+df_answer_wide4$kmodes <- clu_res$clu
+df_answer_wide4 %.>% count(., kmodes)
+df_answer_wide4 %.>% dplyr::select(., one_of('name', 'kmodes')) -> zdf5
+
+tfunc <- function(x){median(x, na.rm = TRUE)}
+
+df_answer_wide4[, -c(1)] %.>% 
+  group_by(., kmodes) %.>% 
+  summarise_all(., tfunc) -> zdf0
+
+zdf0 %.>% 
+  gather(., key = "q", value = "median", -c(clu)) %.>% 
+  spread(., key = clu, value = median) -> zdf1
+
+zdf1 %.>% 
+  rename(.,
+         q_id = q, clu1 = '1', clu2 = '2'
+  ) %.>% 
+  mutate(.,
+         dff = clu1 - clu2
+  ) -> zdf2
+
+zdf2 %.>% count(., dff)
+
+zdf2 %.>% filter(., abs(dff) >= 1) -> zdf3
+zdf3 %.>% count(., clu1)
+zdf3 %.>% count(., clu2)
+zdf2 %.>% filter(., dff %in% c(-1, 1.5)) -> zdf4
+
+
